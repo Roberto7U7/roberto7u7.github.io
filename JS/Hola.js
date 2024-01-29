238670085d8941642a36f9824d82dc7042a46fbc
Roberto7U7 <sebastianzeledon05@gmail.com>
2024-01-28 20:22:30 -0600

    Hola.js

--- JS/Hola.js
+++ JS/Hola.js
@@ -1,19 +1,26 @@
-document.getElementById("botonNo").addEventListener("mouseover", function() {
+var botonNo = document.getElementById("botonNo");
+
+// Para computadoras
+botonNo.addEventListener("mouseover", function() {
     moverBotonNo();
-  });
-  
-  document.getElementById("botonSi").addEventListener("click", function() {
+});
+
+// Para dispositivos táctiles (teléfonos, tablets, etc.)
+botonNo.addEventListener("touchstart", function(event) {
+    event.preventDefault(); // Evita el comportamiento táctil predeterminado (como el desplazamiento)
+    moverBotonNo();
+});
+
+document.getElementById("botonSi").addEventListener("click", function() {
     mostrarMensajeAmor();
-  });
-  
-  function moverBotonNo() {
-    var botonNo = document.getElementById("botonNo");
+});
+
+function moverBotonNo() {
     botonNo.style.position = "absolute";
     botonNo.style.left = Math.random() * (window.innerWidth - botonNo.clientWidth) + "px";
     botonNo.style.top = Math.random() * (window.innerHeight - botonNo.clientHeight) + "px";
-  }
-  
-  function mostrarMensajeAmor() {
-    window.location.href = "/amo.html"; 
-  }
-  
+}
+
+function mostrarMensajeAmor() {
+    window.location.href = "/amo.html";
+}
